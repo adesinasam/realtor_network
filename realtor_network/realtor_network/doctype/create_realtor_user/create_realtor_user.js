610b09c86da138801343e0c38a1ff9e2ad0f08fe
Adesina Akinyemi <59088328+adesinasam@users.noreply.github.com>
2025-06-20 04:15:52 +0100

    create Realtor User

---
+++ realtor_network/realtor_network/doctype/create_realtor_user/create_realtor_user.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Glistercp Solutions and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Create Realtor User", {
+// 	refresh(frm) {
+
+// 	},
+// });
